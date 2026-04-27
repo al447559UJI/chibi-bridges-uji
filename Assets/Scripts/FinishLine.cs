dce7d4fc49309c08d857f213407d8df023985402
Adrià Sánchez López <adrialol68@gmail.com>
2026-04-27 16:22:14 +0200

    Add FinishLine Prefab GameObject

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using Unity.Cinemachine;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLine : MonoBehaviour
+{
+    [SerializeField] AudioClip finishSound;
+    private bool playing = false;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!playing)
+        {
+            StartCoroutine(InitializeFinishBehavior());
+        }
+    }
+
+    private IEnumerator InitializeFinishBehavior()
+    {
+        playing = true;
+        Transform origin = gameObject.transform;
+        CinemachineCamera camera = GameObject.FindGameObjectWithTag("CinemachineCamera").GetComponent<CinemachineCamera>();
+        camera.Follow = origin;
+        SoundManager.instance.PlaySound(finishSound, origin.position);
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
